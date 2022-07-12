e6844b04a5484a169355b5ccdbc25a362448eda1
SaRTaK_B <90795608+SARTHAK2511@users.noreply.github.com>
2022-07-12 18:16:56 +0530

    Update urls.py

--- ModelDeploy/ModelDeploy/urls.py
+++ ModelDeploy/ModelDeploy/urls.py
@@ -17,7 +17,7 @@ from django.contrib import admin
 from django.urls import path,include
 from DjangoAPI import views
 from django.views.static import serve
-from django.conf.urls import url 
+
 from django.conf import settings
 
 urlpatterns = [
